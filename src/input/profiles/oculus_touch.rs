testerpester2/xrizer

commit 6007bd25676bb6430f98c4280e4bf73265bc731c

input: move interaction profile properties into a struct
Feels right.

src/input/profiles/oculus_touch.rs
@@ -1,21 +1,16 @@
-use super::{InteractionProfile, PathTranslation, StringToPath};
+use super::{InteractionProfile, PathTranslation, ProfileProperties, Property, StringToPath};
 use crate::input::legacy::LegacyBindings;
-use crate::openxr_data::Hand;
-use std::ffi::CStr;
-
 pub struct Touch;
 
 impl InteractionProfile for Touch {
-    fn openvr_controller_type(&self) -> &'static CStr {
-        c"oculus_touch"
-    }
-    fn model(&self) -> &'static CStr {
-        c"Miramar"
-    }
-    fn render_model_name(&self, hand: Hand) -> &'static CStr {
-        match hand {
-            Hand::Left => c"oculus_quest_controller_left",
-            Hand::Right => c"oculus_quest_controller_right",
+    fn properties(&self) -> &'static ProfileProperties {
+        &ProfileProperties {
+            model: c"oculus_touch",
+            openvr_controller_type: c"Miramar",
+            render_model_name: Property::PerHand {
+                left: c"oculus_quest_controller_left",
+                right: c"oculus_quest_controller_right",
+            },
         }
     }
     fn profile_path(&self) -> &'static str {

src/input/profiles/vive_controller.rs
@@ -1,19 +1,15 @@
-use super::{InteractionProfile, PathTranslation, StringToPath};
+use super::{InteractionProfile, PathTranslation, ProfileProperties, Property, StringToPath};
 use crate::input::legacy::LegacyBindings;
-use crate::openxr_data::Hand;
-use std::ffi::CStr;
 
 pub struct ViveWands;
 
 impl InteractionProfile for ViveWands {
-    fn openvr_controller_type(&self) -> &'static CStr {
-        c"vive_controller"
-    }
-    fn model(&self) -> &'static CStr {
-        c"Vive. Controller MV"
-    }
-    fn render_model_name(&self, _: Hand) -> &'static CStr {
-        c"vr_controller_vive_1_5"
+    fn properties(&self) -> &'static ProfileProperties {
+        &ProfileProperties {
+            model: c"vive_controller",
+            openvr_controller_type: c"Vive. Controller MV",
+            render_model_name: Property::BothHands(c"vr_controller_vive_1_5"),
+        }
     }
     fn profile_path(&self) -> &'static str {
         "/interaction_profiles/htc/vive_controller"

src/input.rs
@@ -1078,12 +1078,8 @@ impl<C: openxr_data::Compositor> Input<C> {
         hand: Hand,
         property: vr::ETrackedDeviceProperty,
     ) -> Option<&'static CStr> {
-        struct ProfileData {
-            controller_type: &'static CStr,
-            model_number: &'static CStr,
-            render_model_name: &'static CStr,
-        }
-        static PROFILE_MAP: OnceLock<HashMap<xr::Path, ProfileData>> = OnceLock::new();
+        static PROFILE_MAP: OnceLock<HashMap<xr::Path, &'static profiles::ProfileProperties>> =
+            OnceLock::new();
         let get_profile_data = || {
             let map = PROFILE_MAP.get_or_init(|| {
                 let instance = &self.openxr.instance;
@@ -1092,11 +1088,7 @@ impl<C: openxr_data::Compositor> Input<C> {
                     .map(|profile| {
                         (
                             instance.string_to_path(profile.profile_path()).unwrap(),
-                            ProfileData {
-                                controller_type: profile.openvr_controller_type(),
-                                model_number: profile.model(),
-                                render_model_name: profile.render_model_name(hand),
-                            },
+                            profile.properties(),
                         )
                     })
                     .collect()
@@ -1112,16 +1104,16 @@ impl<C: openxr_data::Compositor> Input<C> {
         match property {
             // Audica likes to apply controller specific tweaks via this property
             vr::ETrackedDeviceProperty::ControllerType_String => {
-                get_profile_data().map(|data| data.controller_type)
+                get_profile_data().map(|data| data.openvr_controller_type)
             }
             // I Expect You To Die 3 identifies controllers with this property -
             // why it couldn't just use ControllerType instead is beyond me...
             vr::ETrackedDeviceProperty::ModelNumber_String => {
-                get_profile_data().map(|data| data.model_number)
+                get_profile_data().map(|data| data.model)
             }
             // Resonite won't recognize controllers without this
             vr::ETrackedDeviceProperty::RenderModelName_String => {
-                get_profile_data().map(|data| data.render_model_name)
+                get_profile_data().map(|data| *data.render_model_name.get(hand))
             }
             // Required for controllers to be acknowledged in I Expect You To Die 3
             vr::ETrackedDeviceProperty::SerialNumber_String

src/input/profiles.rs
@@ -15,15 +15,7 @@ use vive_controller::ViveWands;
 #[allow(private_interfaces)]
 pub trait InteractionProfile: Sync + Send {
     fn profile_path(&self) -> &'static str;
-    /// Corresponds to Prop_ModelNumber_String
-    /// Can be pulled from a SteamVR System Report
-    fn model(&self) -> &'static CStr;
-    /// Corresponds to Prop_ControllerType_String
-    /// Can be pulled from a SteamVR System Report
-    fn openvr_controller_type(&self) -> &'static CStr;
-    /// Corresponds to RenderModelName_String
-    /// Can be found in SteamVR under resources/rendermodels (some are in driver subdirs)
-    fn render_model_name(&self, _: Hand) -> &'static CStr;
+    fn properties(&self) -> &'static ProfileProperties;
     fn translate_map(&self) -> &'static [PathTranslation];
 
     fn legal_paths(&self) -> Box<[String]>;
@@ -31,6 +23,35 @@ pub trait InteractionProfile: Sync + Send {
     fn offset_grip_pose(&self, pose: xr::Posef) -> xr::Posef {
         pose
     }
+}
+
+pub enum Property<T> {
+    BothHands(T),
+    PerHand { left: T, right: T },
+}
+
+impl<T> Property<T> {
+    pub fn get(&self, hand: Hand) -> &T {
+        match self {
+            Self::BothHands(property) => property,
+            Self::PerHand { left, right } => match hand {
+                Hand::Left => left,
+                Hand::Right => right,
+            },
+        }
+    }
+}
+
+pub struct ProfileProperties {
+    /// Corresponds to Prop_ModelNumber_String
+    /// Can be pulled from a SteamVR System Report
+    pub model: &'static CStr,
+    /// Corresponds to Prop_ControllerType_String
+    /// Can be pulled from a SteamVR System Report
+    pub openvr_controller_type: &'static CStr,
+    /// Corresponds to RenderModelName_String
+    /// Can be found in SteamVR under resources/rendermodels (some are in driver subdirs)
+    pub render_model_name: Property<&'static CStr>,
 }
 
 pub(super) struct PathTranslation {

src/input/profiles/knuckles.rs
@@ -1,9 +1,7 @@
-use super::{InteractionProfile, PathTranslation, StringToPath};
+use super::{InteractionProfile, PathTranslation, ProfileProperties, Property, StringToPath};
 use crate::input::legacy::LegacyBindings;
-use crate::openxr_data::Hand;
 use openxr as xr;
 use std::f32::consts::FRAC_PI_6;
-use std::ffi::CStr;
 
 pub struct Knuckles;
 
@@ -11,16 +9,14 @@ impl InteractionProfile for Knuckles {
     fn profile_path(&self) -> &'static str {
         "/interaction_profiles/valve/index_controller"
     }
-    fn model(&self) -> &'static CStr {
-        c"Knuckles"
-    }
-    fn openvr_controller_type(&self) -> &'static CStr {
-        c"knuckles"
-    }
-    fn render_model_name(&self, hand: Hand) -> &'static CStr {
-        match hand {
-            Hand::Left => c"valve_controller_knu_1_0_left",
-            Hand::Right => c"valve_controller_knu_1_0_right",
+    fn properties(&self) -> &'static ProfileProperties {
+        &ProfileProperties {
+            model: c"Knuckles",
+            openvr_controller_type: c"knuckles",
+            render_model_name: Property::PerHand {
+                left: c"valve_controller_knu_1_0_left",
+                right: c"valve_controller_knu_1_0_right",
+            },
         }
     }
     fn translate_map(&self) -> &'static [PathTranslation] {

src/input/profiles/simple_controller.rs
@@ -1,19 +1,15 @@
-use super::{InteractionProfile, PathTranslation, StringToPath};
+use super::{InteractionProfile, PathTranslation, ProfileProperties, Property, StringToPath};
 use crate::input::legacy::LegacyBindings;
-use crate::openxr_data::Hand;
-use std::ffi::CStr;
 
 pub struct SimpleController;
 
 impl InteractionProfile for SimpleController {
-    fn openvr_controller_type(&self) -> &'static CStr {
-        c"generic" // meaningless really
-    }
-    fn model(&self) -> &'static CStr {
-        c"<unknown>"
-    }
-    fn render_model_name(&self, _: Hand) -> &'static CStr {
-        c"generic_controller"
+    fn properties(&self) -> &'static ProfileProperties {
+        &ProfileProperties {
+            model: c"generic",
+            openvr_controller_type: c"<unknown>",
+            render_model_name: Property::BothHands(c"generic_controller"),
+        }
     }
     fn profile_path(&self) -> &'static str {
         "/interaction_profiles/khr/simple_controller"

commit aac3123a22fd54dabd1c864be654253a2388f30f

Fix Wands and Oculus Touch properties
Caused by 6007bd25676bb6430f98c4280e4bf73265bc731c.
Oopsie.

src/input/profiles/oculus_touch.rs
@@ -11,8 +11,8 @@ pub struct Touch;
 impl InteractionProfile for Touch {
     fn properties(&self) -> &'static ProfileProperties {
         &ProfileProperties {
-            model: c"oculus_touch",
-            openvr_controller_type: c"Miramar",
+            model: c"Miramar",
+            openvr_controller_type: c"oculus_touch",
             render_model_name: Property::PerHand {
                 left: c"oculus_quest_controller_left",
                 right: c"oculus_quest_controller_right",

src/input/profiles/vive_controller.rs
@@ -11,8 +11,8 @@ pub struct ViveWands;
 impl InteractionProfile for ViveWands {
     fn properties(&self) -> &'static ProfileProperties {
         &ProfileProperties {
-            model: c"vive_controller",
-            openvr_controller_type: c"Vive. Controller MV",
+            model: c"Vive. Controller MV",
+            openvr_controller_type: c"vive_controller",
             render_model_name: Property::BothHands(c"vr_controller_vive_1_5"),
             has_joystick: false,
             has_trackpad: true,